2c12526907c1ae5153913946ad76808c96058385
Themba <tfmzworld@gmail.com>
2020-05-05 11:38:05 +0200

    Add Delete button design

--- src/Components/Todo/index.js
+++ src/Components/Todo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ClearIcon from '@material-ui/icons/Clear';
 
 import './style.css'
 import NewFormTodo from '../NewFormTodo';
@@ -12,9 +13,9 @@ const Todo = ({ editMode, id, removeTodo, isDone, editTodo, handleUpdate, name }
                     <NewFormTodo name={name} id={id} handleSubmit={handleUpdate} /> :
                     <div className="todo-item_inner">
                         <div className="todo-removeBtn">
-                            <button onClick={() => removeTodo(id)}>x</button>
+                            <button onClick={() => removeTodo(id)}>X</button>
                         </div>
-                        <h2>{name}</h2>
+                        <h3>{name}</h3>
                         <div className="todo-btns">
                             <button onClick={() => removeTodo(id)}>done</button>
                             <button onClick={() => editTodo(id)}>edit</button>
